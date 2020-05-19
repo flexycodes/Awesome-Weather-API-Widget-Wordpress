2f004e5ed632ac27084e9b3119e5db7729f5b12c
Flexycodes <mac@Macs-MacBook-Pro.local>
2020-05-19 21:53:15 +0000

    merge local with repository

---
+++ fc-weather-api.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Awesome Weather API Widget
+Plugin URI: https://themeforest.net/user/flexycodes
+Description: An API weather widget for the dashboard
+Version: 1.0.0
+Author: Flexycodes
+Author URI: https://themeforest.net/user/flexycodes
+Text Domain: wep
+*/
+
+ini_set('max_execution_time', 0);
